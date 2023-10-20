1bf429ab3a780a820aa7dfec5928efbc7a02d2a3
SahidAnsari <SahidAnsari@DESKTOP-QI1M344>
2023-10-20 12:40:43 +0530

    final commit 20_10_2023

--- DemoProperties-1/src/main/java/com/finzly/demo/service/PropertiesService.java
+++ DemoProperties-1/src/main/java/com/finzly/demo/service/PropertiesService.java
@@ -1,13 +1,11 @@
 package com.finzly.demo.service;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-import java.util.Scanner;
-
+import java.io.BufferedInputStream;
+import java.util.HashMap;
+import java.util.*;
+import java.util.Map.Entry;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 import com.finzly.demo.entity.PropertiesManager;
@@ -19,62 +17,97 @@ public class PropertiesService {
 	@Autowired
 	PropertiesRepository propertiesRepository;
 
-	@Autowired
-	private ResourceLoader resourceLoader;
 
-	public String readFileFromResources(String fileName) throws IOException {
-		Resource resource = resourceLoader.getResource("classpath:" + fileName);
-		Properties properties = new Properties();
-		
-		try (InputStream inputStream = resource.getInputStream()) {
+//	@Autowired
+//	private ResourceLoader resourceLoader;
+	
+	private Map<String , String> mappedKeyValue = new HashMap<>();
+
+	public Map<String, String> readFileFromResources(String fileName) {
+//		Resource resource = resourceLoader.getResource("classpath:" + fileName);
+//		Properties properties = new Properties();
 			
-//			Scanner scanner = new Scanner(inputStream)) {
-//			StringBuilder content = new StringBuilder();
-//			while (scanner.hasNextLine()) {
-//				content.append(scanner.nextLine()).append("\n");
+		try {
+//			Method 0
+			BufferedInputStream bis = new BufferedInputStream(new ClassPathResource(fileName).getInputStream());
+			int characterInBis;			
+			StringBuilder currentLine = new StringBuilder();
+
+            while ((characterInBis = bis.read()) != -1) {
+                char currentChar = (char) characterInBis;
+                if (currentChar == '\n') {
+                    String[] keyValue = currentLine.toString().split("=");
+                    if (keyValue.length == 2) {
+                        String key = keyValue[0];
+                        String value = keyValue[1];
+                        System.out.println("Key: " + key + "\nValue: " + value);
+                        mappedKeyValue.put(key, value);
+                    }
+                    currentLine.setLength(0); // Clear the current line buffer
+                } else {
+                    currentLine.append(currentChar);
+                }
+            }
+			
+//          Method 1
+//			StringBuilder sb = new StringBuilder();
+//			while((characterInBis = bis.read()) != -1) {
+//				sb.append((char) characterInBis);
 //			}
-//			seperatorByEquals(content);
+//						
+//			for(String line: sb.toString().split("\n")){
+//				String[] keyValue = line.split("=");
+//				if(keyValue.length == 2) {
+//					String key = keyValue[0];
+//					String value = keyValue[1];
+////					System.out.println("Key: "+key+"\nValue: "+value);
+//					mappedKeyValue.put(key, value);
+//				}
+//			}
 			
-			properties.load(inputStream);
-//			System.out.println(properties.toString());
-			for (String name : properties.stringPropertyNames()) {
-				String value = properties.getProperty(name);
-//				System.out.println(name);
-//				System.out.println(value);
-				
-				PropertiesManager propertiesManager = new PropertiesManager();
-				
-				propertiesManager.setPropertiesKeys(name);
-				propertiesManager.setPropertiesValues(value);
-				
-				propertiesRepository.save(propertiesManager);
-			}
-			return "Added";
+//          Method 2
+//			InputStream is = res.getInputStream();
+//			BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+//			
+//			String line;
+//			while((line = reader.readLine()) != null){
+//				String[] content = line.split("=");
+//				if(content.length == 2) {
+//					String key = content[0];
+//					String value = content[1];
+//					mappedKeyValue.put(key, value);
+//				}
+//			}
+			
+			
+//          Method 3
+//			InputStream inputStream = resource.getInputStream();
+//			properties.load(inputStream);
+//			System.out.println(inputStream);
+//			
+//			for (String name : properties.stringPropertyNames()) {
+//				String value = properties.getProperty(name);				
+//				mappedKeyValue.put(name, value);				
+//			}
+			
+			
+			return addingToDataBase(mappedKeyValue);
 		}catch (Exception e) {
-			return ""+e.getMessage();
+			return null;
 		}
 	}
+	
+	
+	public Map<String, String> addingToDataBase(Map<String, String> mappedKeyValue) {
+		for (Entry<String, String> entry : mappedKeyValue.entrySet()) {
+			PropertiesManager p = new PropertiesManager();
+			p.setPropertiesKeys(entry.getKey());
+			p.setPropertiesValues(entry.getValue());
+			propertiesRepository.save(p);
+	    }
+		return mappedKeyValue;
+		
+	}
+	
 
-//	public void seperatorByEquals(StringBuilder content) {
-//		String c = content.toString();
-//		String[] contentsLine = c.split("\n");
-//		String key;
-//		String value;
-//
-//		for (String string : contentsLine) {
-//			String[] keyValuePair = string.split("=");
-//			if (keyValuePair.length == 2) {
-//				key = keyValuePair[0].trim();
-//				value = keyValuePair[1].trim();
-//				addingToDataBase(key, value);
-//			}
-//		}
-//	}
-//
-//	public void addingToDataBase(String key, String value) {
-//		PropertiesManager p = new PropertiesManager();
-//		p.setPropertiesKeys(key);
-//		p.setPropertiesValues(value);
-//		propertiesRepository.save(p);
-//	}
 }
